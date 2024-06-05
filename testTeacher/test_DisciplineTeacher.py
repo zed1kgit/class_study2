292d035f496b0e6c7cd8a1768cbb651ae6ddbf38
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-06 00:40:17 +0300

    Added getters test for class DisciplineTeacher

--- testTeacher/test_DisciplineTeacher.py
+++ testTeacher/test_DisciplineTeacher.py
@@ -3,3 +3,16 @@ from main import DisciplineTeacher
 
 def test_teacher_init(dis_teacher):
     assert len(DisciplineTeacher.teachers_dict) == 1
+
+
+def test_get_discipline(dis_teacher):
+    assert dis_teacher.get_discipline() == 'test_discipline'
+
+
+def test_get_job_title(dis_teacher):
+    assert dis_teacher.get_job_title() == 'test_job_tittle'
+
+
+def test_get_teacher_data(dis_teacher):
+    assert dis_teacher.get_teacher_data() == ('test_name, образование test_education, опыт работы test_experience.\n'
+                                              'Предмет test_discipline, должность test_job_tittle')
