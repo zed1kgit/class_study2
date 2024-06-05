4b945672072b0b94742044338e05a02e3cd7e488
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-05 23:52:04 +0300

    Added getters test for class Teacher

--- testTeacher/testTeacher.py
+++ testTeacher/testTeacher.py
@@ -4,3 +4,15 @@ from main import Teacher
 def test_teacher_init(teacher):
     assert len(Teacher.teachers_dict) == 1
     assert Teacher.teachers_dict == {'test_name': ['test_education', 'test_experience']}
+
+
+def test_get_name(teacher):
+    assert teacher.get_name() == 'test_name'
+
+
+def test_get_education(teacher):
+    assert teacher.get_education() == 'test_education'
+
+
+def test_get_experience(teacher):
+    assert teacher.get_experience() == 'test_experience'
