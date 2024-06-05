a4ca99612a671d7e458586042c6f42e687a3e6c4
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-06 00:50:40 +0300

    Added give_a_consultation test for class DisciplineTeacher

--- testTeacher/test_DisciplineTeacher.py
+++ testTeacher/test_DisciplineTeacher.py
@@ -24,3 +24,8 @@ def test_add_mark(dis_teacher):
 
 def test_remove_mark(dis_teacher):
     assert dis_teacher.remove_mark("test", "5") == 'test_name удалил оценку 5 студенту test.\nПредмет test_discipline'
+
+
+def test_give_a_consultation(dis_teacher):
+    assert dis_teacher.give_a_consultation('test_ed_class') == ('test_name провел консультацию в классе test_ed_class.'
+                                                                '\nПо предмету test_discipline как test_job_tittle')
